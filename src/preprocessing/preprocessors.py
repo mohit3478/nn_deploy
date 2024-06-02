cf75672dd26e0d780ea174ec2445a39affcd5dc6
mohit3478 <audchiyamohit@gmail.com>
2024-06-02 10:41:42 +0530

    Modified data_management.py and added preprocessors.py

--- src/preprocessing/preprocessors.py
+++ src/preprocessing/preprocessors.py
@@ -1,8 +1,29 @@
-import numpy as np
+import numpy as np 
 import os
-
-from src.config import config
+from src.config import config 
 
 class preprocess_data:
     
-    
+    def fit(self,X,y=None):
+        
+        self.num_rows =X.shape[0]
+        self.num_input_features = X.shape[1]
+        
+        if len(X.shape) == 1:
+          self.target_feature_dim = 1
+        else:
+            self.target_feature_dim = X.shape[1]
+        
+        if len(y.shape) == 1:
+          self.target_feature_dim = 1
+        else:
+            self.target_feature_dim = y.shape[1]
+        
+        
+    def transfrom(self ,X=None,y=None):
+        
+         
+        self.X = np.array(X).reshape(self.num_rows,self.num_input_features)
+        self.Y = np.array(y).reshape(self.num_rows,self.target_feature_dim)
+        
+        return self.X, self.Y
