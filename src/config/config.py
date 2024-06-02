132ccf7a7ac42e0f2a6c8293d9bce4a047d06a61
mohit3478 <audchiyamohit@gmail.com>
2024-06-02 11:48:20 +0530

    Modified data_management.py and added preprocessors.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,36 +1,18 @@
 import os
 import pathlib
 import src
-
 NUM_INPUTS = 2
 NUM_LAYERS = 3
 P = [NUM_INPUTS]
-
-
 
 f = [None,"linear","sigmoid"]
 
 LOSS_FUNCTION = "Mean Squared Error"
 MINI_BATCH_SIZE = 1
 
-PACKAGE_ROOT = pathlib.Path(src.__file__).resolve().parent
-
+PACKAGE_ROOT = pathlib.Path(src._file_).reslove().parent
 DATAPATH = os.path.join(PACKAGE_ROOT,"datasets")
+#"/src/datastes"
+SAVE_MODEL_PATH = os.path.join(PACKAGE_ROOT,"trained_model")
+#"/src/trained_models"
 
-SAVED_MODEL_PATH = os.path.join(PACKAGE_ROOT,"trained_models")
-
-
-#theta0 = [None]
-#theta = [None]
-
-#z = [None]*NUM_LAYERS
-#h = [None]*NUM_LAYERS
-
-"""
-del_fl_by_del_z = [None]*NUM_LAYERS
-del_hl_by_del_theta0 = [None]*NUM_LAYERS
-del_hl_by_del_theta = [None]*NUM_LAYERS
-del_L_by_del_h = [None]*NUM_LAYERS
-del_L_by_del_theta0 = [None]*NUM_LAYERS
-del_L_by_del_theta = [None]*NUM_LAYERS
-"""
